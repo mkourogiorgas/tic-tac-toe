814a3157a93bba96b99d839dd6a23a820a54d0bd
mixos <mkourogiorgas@gmail.com>
2017-12-08 12:28:01 +0200

    render symbol components as square children

--- src/Game.js
+++ src/Game.js
@@ -1,20 +1,28 @@
 import React from 'react';
 import Square from './Square';
+import {SymbolO, SymbolX} from './Symbols';
 import './App.css';
 
 class Game extends React.Component {
+
 
 	handleSquareClick = (element) => {
     element.currentTarget.style.backgroundColor = 'green';
 	}
 
 	render() {
+		let symbolsToRender = Array(9);
+		for (let i=0; i<symbolsToRender.length; i++) {
+			symbolsToRender[i] = <SymbolO/>;
+		}
+
 		return (
 			<div className='game'>
 				{[...Array(9)].map((x, i) =>
 					<Square key={i} id={i} click={this.handleSquareClick.bind(this)} >
+						{symbolsToRender[i]}
 					</Square>
- 				)}
+				)}
 			</div>
 		);
 	}
--- src/Square.js
+++ src/Square.js
@@ -5,6 +5,7 @@ class Square extends React.Component {
 	render() {
 		return (
 			<div id={this.props.id} className='square' onClick={this.props.click}>
+				{this.props.children}
 			</div>
 		);
 	}
